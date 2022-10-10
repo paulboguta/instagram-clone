e9ee03f58c33f125bba5574ed8b14c5ff85d6f6f
paulboguta <paul.boguta@gmail.com>
2022-10-10 13:06:41 +0200

    fix redux actions bug, users can like posts of others too now

--- src/store/actions/postActions.tsx
+++ src/store/actions/postActions.tsx
@@ -1,4 +1,13 @@
-import { collection, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  collectionGroup,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../../services/firebase";
 import { AppDispatch } from "../hooks";
 
@@ -28,6 +37,7 @@ export const addPost =
       profilePic: profilePic,
       id: postID,
       dateAdded: date,
+      uid: uid,
     });
     dispatch({
       type: ADD_POST,
@@ -81,24 +91,34 @@ export const likePost =
     const username = userData.data()!.username;
     const profilePic = userData.data()!.profilePic;
 
-    const postRef = doc(db, `users/${uid}/posts/`, id);
-    const postData = await getDoc(postRef);
+    const allPosts = query(collectionGroup(db, "posts"));
+    const querySnapshot = await getDocs(allPosts);
 
     let arr: any[] = [];
-    if (postData.data()!.likes.length > 0) {
-      postData.data()!.likes.map((like: any[]) => {
-        arr.push(like);
+    let userPost: string;
+    const manageData = async () => {
+      querySnapshot.forEach((doc) => {
+        if (doc.id === id) {
+          if (doc.data()!.likes.length > 0) {
+            arr = doc.data()!.likes;
+          }
+          arr.push({ uid: uid, username: username, profilePic: profilePic });
+          userPost = doc.data()!.uid;
+        }
+        return;
       });
-    }
-    arr.push({ uid: uid, username: username, profilePic: profilePic });
+      const postRef = doc(db, `users/${userPost}/posts`, id);
 
-    await setDoc(
-      postRef,
-      {
-        likes: arr,
-      },
-      { merge: true }
-    );
+      await setDoc(
+        postRef,
+        {
+          likes: arr,
+        },
+        { merge: true }
+      );
+    };
+
+    manageData();
 
     dispatch({
       type: LIKE_POST,
@@ -108,26 +128,38 @@ export const likePost =
 
 export const unlikePost =
   (uid: string, id: string) => async (dispatch: AppDispatch) => {
-    const postRef = doc(db, `users/${uid}/posts/`, id);
-    const postData = await getDoc(postRef);
+    const allPosts = query(collectionGroup(db, "posts"));
+    const querySnapshot = await getDocs(allPosts);
 
-    const likes = postData.data()!.likes.filter((like: any) => {
-      if (like.uid !== uid) {
-        return like;
-      }
-    });
+    let arr: any[] = [];
+    let userPost: string;
+    const manageData = async () => {
+      querySnapshot.forEach((doc) => {
+        if (doc.data()!.id === id) {
+          userPost = doc.data()!.uid;
+          doc.data()!.likes.filter((like: any) => {
+            if (like.uid !== uid) {
+              arr.push(like);
+            }
+          });
+        }
+      });
+      const postRef = doc(db, `users/${userPost}/posts`, id);
 
-    await setDoc(
-      postRef,
-      {
-        likes: likes,
-      },
-      { merge: true }
-    );
+      await setDoc(
+        postRef,
+        {
+          likes: arr,
+        },
+        { merge: true }
+      );
+    };
+
+    manageData();
 
     dispatch({
       type: UNLIKE_POST,
-      likes: likes,
+      likes: arr,
       id: id,
     });
   };
