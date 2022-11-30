a3b675bb957490ee6ebf64c4f62c45622676dd52
paulboguta <paul.boguta@gmail.com>
2022-11-30 14:18:56 +0100

    fix bug profile posts navigation

--- src/components/post/ProfilePost/ProfilePost.tsx
+++ src/components/post/ProfilePost/ProfilePost.tsx
@@ -3,15 +3,16 @@ import { Wrapper } from "./ProfilePost.styles";
 
 interface IProfilePostProps {
   src: string;
+  id: string;
 }
 
-export const ProfilePost = ({ src }: IProfilePostProps) => {
+export const ProfilePost = ({ src, id }: IProfilePostProps) => {
   const navigate = useNavigate();
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     navigate(`/post/${event.currentTarget.id}`);
   };
   return (
-    <Wrapper onClick={onClick}>
+    <Wrapper onClick={onClick} id={id}>
       <img src={src} alt="profile img" />
     </Wrapper>
   );
