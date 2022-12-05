0b0398863c9bec37abba04e695855903fbafe747
paulboguta <paul.boguta@gmail.com>
2022-12-05 16:49:15 +0100

    fix input color dark mode

--- src/components/post/CommentsWrapper/CommentInput.tsx
+++ src/components/post/CommentsWrapper/CommentInput.tsx
@@ -1,10 +1,16 @@
 import { ChangeEvent } from "react";
-import { CommentInputStyled } from "./CommentsWrapper.styles";
+import styled from "styled-components";
 
 interface ICommentInput {
   newComment: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
+const CommentInputStyled = styled.input`
+  background-color: ${(props) => props.theme.backgroundPrimary};
+  color: ${(props) => props.theme.fontPrimary};
+  border: none;
+  outline: none;
+`;
 
 export const CommentInput = ({ newComment, onChange }: ICommentInput) => {
   return (
