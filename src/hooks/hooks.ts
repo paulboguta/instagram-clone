336297dfa6dec9daf70fa73be8d9c5678dbaf4eb
paulboguta <paul.boguta@gmail.com>
2022-12-08 14:39:24 +0100

    remove unused custom hooks

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,24 +1,4 @@
-import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
-import { auth } from "../services/firebase";
-
-export const useToggle = (defaultValue: boolean) => {
-  const [value, setValue] = useState(defaultValue);
-
-  const toggle = () => setValue(!value);
-
-  return [value, toggle];
-};
-
-export const useAuth = () => {
-  const [currentUser, setCurrentUser] = useState<any>();
-
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => setCurrentUser(user));
-    return unsub;
-  }, []);
-  return currentUser;
-};
 
 const getWindowDimensions = () => {
   const { innerWidth: width, innerHeight: height } = window;
